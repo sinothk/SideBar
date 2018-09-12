3f29c355509027ce26232ae4dad9a7006b27ff9f
LiangYutao <sinothk@126.com>
2018-09-12 13:55:55 +0800

    新增Style1

--- app/src/main/java/com/sinothk/sidebar/demo/MainActivity.java
+++ app/src/main/java/com/sinothk/sidebar/demo/MainActivity.java
@@ -1,7 +1,11 @@
 package com.sinothk.sidebar.demo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.sinothk.sidebar.demo.style1.SideBarDemoMainActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +14,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void gotoSideBarDemoMainActivity(View view) {
+        startActivity(new Intent(this, SideBarDemoMainActivity.class));
+    }
+
+//    public void gotoSwitchTabDemoActivity(View view) {
+//        startActivity(new Intent(this, SwitchTabDemoActivity.class));
+//    }
+//
+//    public void gotoMallDetailImageView(View view) {
+//        startActivity(new Intent(this, MallDemoImageViewMainActivity.class));
+//    }
+//
+//    public void gotoBannerViewMainActivity(View view) {
+//        startActivity(new Intent(this, BannerViewMainActivity.class));
+//    }
+//
+//
+//    public void gotoSideBarDemoMainActivity(View view) {
+//        startActivity(new Intent(this, SideBarDemoMainActivity.class));
+//    }
 }
---
+++ app/src/main/java/com/sinothk/sidebar/demo/style1/SideBarDemoMainActivity.java
@@ -0,0 +1,54 @@
+package com.sinothk.sidebar.demo.style1;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.sinothk.sidebar.demo.R;
+import com.sinothk.widget.sidebar.style1.WaveSideBar;
+
+import java.util.ArrayList;
+
+public class SideBarDemoMainActivity extends AppCompatActivity {
+    private RecyclerView rvContacts;
+    private WaveSideBar sideBar;
+
+    private ArrayList<Contact> contacts = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        initData();
+        initView();
+    }
+
+    private void initView() {
+        setContentView(R.layout.side_bar_activity_main);
+        rvContacts = (RecyclerView) findViewById(R.id.rv_contacts);
+        rvContacts.setLayoutManager(new LinearLayoutManager(this));
+
+        rvContacts.setAdapter(new ContactsAdapter(contacts, R.layout.side_bar_item_contacts));
+
+        sideBar = (WaveSideBar) findViewById(R.id.side_bar);
+        sideBar.setIndexItems("A", "B", "D", "F", "H", "#");
+        sideBar.setPosition(WaveSideBar.POSITION_RIGHT);
+        sideBar.setOnSelectIndexItemListener(new WaveSideBar.OnSelectIndexItemListener() {
+            @Override
+            public void onSelectIndexItem(String index) {
+                for (int i = 0; i < contacts.size(); i++) {
+                    if (contacts.get(i).getIndex().equals(index)) {
+                        ((LinearLayoutManager) rvContacts.getLayoutManager()).scrollToPositionWithOffset(i, 0);
+                        return;
+                    }else{
+                    }
+                }
+            }
+        });
+    }
+
+    private void initData() {
+        contacts.addAll(Contact.getChineseContacts());
+    }
+}
